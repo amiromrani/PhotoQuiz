ce041aad836b4c59d00a99bc07ca8aa3ed7b7c63
amiromrani <46926811+amiromrani@users.noreply.github.com>
2019-05-14 08:52:25 -0500

    Initial commit

---
+++ app/src/main/java/com/ctech/amir/photoquiz/MainActivity.java
@@ -0,0 +1,30 @@
+package com.ctech.amir.photoquiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+
+    private Button mPhoneButton;
+    private Button mKeyButton;
+    private Button mBottleButton;
+    private Button mSpeakerButton;
+
+
+
+
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
