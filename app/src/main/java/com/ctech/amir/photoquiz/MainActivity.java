4b12936408d4bd963fb5735008e184c0614008f1
amiromrani <46926811+amiromrani@users.noreply.github.com>
2019-05-15 08:52:21 -0500

    make the answers 1234 and the questions the same

--- app/src/main/java/com/ctech/amir/photoquiz/MainActivity.java
+++ app/src/main/java/com/ctech/amir/photoquiz/MainActivity.java
@@ -3,6 +3,7 @@ package com.ctech.amir.photoquiz;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +14,17 @@ public class MainActivity extends AppCompatActivity {
     private Button mKeyButton;
     private Button mBottleButton;
     private Button mSpeakerButton;
+
+
+
+    private TextView mQuestionTextView;
+
+    private Question[] mQuestionBank = new Question[] {
+
+
+            new Question(R.string.bottle_question, mBottleButton)
+    }
+
 
 
 
