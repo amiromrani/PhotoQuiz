0f4eb1ff87392da72fdfe6064a24def6e98ff728
amiromrani <46926811+amiromrani@users.noreply.github.com>
2019-05-20 08:58:23 -0500

    need to add vertical and horizontal layouts

--- app/src/main/java/com/ctech/amir/photoquiz/MainActivity.java
+++ app/src/main/java/com/ctech/amir/photoquiz/MainActivity.java
@@ -3,8 +3,10 @@ package com.ctech.amir.photoquiz;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,9 +22,17 @@ public class MainActivity extends AppCompatActivity {
 
 
     private TextView mQuestionTextView;
+    private Question[] mQuestionBank = new Question[] {
+
+
+            new Question(R.string.bottle_question, true),
+    };
 
 
 
+
+    // the current question being shown
+    private int mCurrentIndex = 0;
 
 
 @Override
@@ -38,8 +48,135 @@ protected void onCreate(Bundle savedInstanceState){
 
 
         mBottleButton = findViewById(R.id.bottle_button);
+        mBottleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                // update the current index, but stay within the length of the array
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                updateQuestion();
+
+
+            }
+        });
+
+        mKeyButton = findViewById(R.id.key_button);
+        mKeyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // update the current index, but stay within the length of the array
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                updateQuestion();
+
+            }
+        });
+
+        mPhoneButton = findViewById(R.id.phone_button);
+        mPhoneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // update the current index, but stay within the length of the array
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                updateQuestion();
+
+            }
+        });
+
+        mSpeakerButton = findViewById(R.id.speaker_button);
+        mSpeakerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // update the current index, but stay within the length of the array
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                updateQuestion();
+
+            }
+        });
+
+
+
+
+
+
+
+
 
 
 
         } // onCreate ends
+
+
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart has been called!");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume has been called!");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause has been called!");
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle saveInstanceState) {
+        super.onSaveInstanceState(saveInstanceState);
+        Log.i(TAG, "onSaveInstanceState");
+        saveInstanceState.putInt(KEY_INDEX, mCurrentIndex);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop has been called!");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy has been called!");
+    }
+
+    // encapsulate the update question code so we don't have to copy and paste it:
+    private void updateQuestion() {
+        Log.d(TAG, "updating question text", new Exception());
+        int questionResourceId = mQuestionBank[mCurrentIndex].getTextResId();
+        mQuestionTextView.setText(questionResourceId);
+    }
+
+
+
+    // check whether the button clicked matches the answer in the resource
+    private void checkAnswer(boolean userPressedTrue) {
+
+        boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
+
+        int messageResourceId = 0;
+
+        if (userPressedTrue == answerIsTrue) {
+            messageResourceId = R.string.correct_toast;
+        } else {
+            messageResourceId = R.string.incorrect_toast;
+        }
+
+        // you can make fancier Toast here if you want (from previous challenge)
+        Toast.makeText(this, messageResourceId, Toast.LENGTH_SHORT).show();
+    }
+
+
+
+
+
 }
